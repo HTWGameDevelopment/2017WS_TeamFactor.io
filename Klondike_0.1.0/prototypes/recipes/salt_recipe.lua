c300b9188042231e9259cfb12677709b34cc8421
lennart.monir <gartenfreund931@aol.com>
2017-11-17 12:11:02 +0100

    Add Salt

--- Klondike_0.1.0/prototypes/recipes/salt_recipe.lua
+++ Klondike_0.1.0/prototypes/recipes/salt_recipe.lua
@@ -4,8 +4,9 @@ data:extend({
 		name = "salt-recipe",
 		category = "chemistry",
 		enabled = true,
-		energy_required = 5
-		ingredients = {
+		energy_required = 5,
+		ingredients =
+		{
 			{type="fluid", name="water", amount=100},			
 		},
 		result = "salt-item",
