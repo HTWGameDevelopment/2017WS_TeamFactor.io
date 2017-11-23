a0fd02f46de2599e2657bcb147cea6d3ca2a1fef
lennart.monir <gartenfreund931@aol.com>
2017-11-23 21:10:52 +0100

    added mill without functions

---
+++ Klondike_0.1.0/prototypes/items/mill_item.lua
@@ -0,0 +1,10 @@
+data:extend({
+	{
+		type = "item",
+		name = "mill-item",
+		icon = "__Klondike__/graphics/icons/mill.png",
+		flags = {"goes-to-main-inventory"},
+		subgroup = "raw-material",		
+		stack_size = 5
+	}
+})
