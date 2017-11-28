b7fb354db57fa90efe6ca5f4dc04c81d79866f15
lmonir <lmonir@htwsaar.de>
2017-11-28 12:34:46 +0100

    Soysouce with colorfunction

--- Klondike_0.1.0/logic/color_function.lua
+++ Klondike_0.1.0/logic/color_function.lua
@@ -1,3 +1,6 @@
+--Es gehen nur 2 Nachkommastellen
+--Berechnung: 1/256*(r||g||b) https://html-color-codes.info
+
 function color(num)
   local mult = 10^(2 or 0)
   local x = 1/256*num
--- Klondike_0.1.0/prototypes/fluids/soysouce_fluid.lua
+++ Klondike_0.1.0/prototypes/fluids/soysouce_fluid.lua
@@ -6,8 +6,8 @@ data:extend(
     default_temperature = 15,
     max_temperature = 100,
     heat_capacity = "0.2KJ",    
-    base_color = {r=0.16, g=0.04, b=0.04}, --Es gehen nur 2 Nachkommastellen
-    flow_color = {r=0.25, g=0.08, b=0.02}, --Berechnung: 1/256*(r||g||b) https://html-color-codes.info
+    base_color = {r=color(41), g=color(10), b=color(10)}, --Es gehen nur 2 Nachkommastellen
+    flow_color = {r=color(64), g=color(20), b=color(41)}, --Berechnung: 1/256*(r||g||b) https://html-color-codes.info
     icon = "__Klondike__/graphics/icons/soysouce-fluid.png",
     order = "a[fluid]-a[water]",
     pressure_to_speed_ratio = 0.4,
