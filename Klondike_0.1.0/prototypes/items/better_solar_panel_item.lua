ee7c4f586ed2c788546363a91406f11e7488156d
Lmonir <lmonir@htwsaar.de>
2017-11-29 14:51:20 +0100

    solar panel

---
+++ Klondike_0.1.0/prototypes/items/better_solar_panel_item.lua
@@ -0,0 +1,11 @@
+data:extend({
+	{
+		type = "item",
+		name = "better-solar-panel",
+		icon = "__Klondike__/graphics/icons/better-solar-panel.png",
+		flags = {"goes-to-main-inventory"},
+		subgroup = "energy",
+		order = "z[better-solar-panel]",		
+		stack_size = 50
+	}
+})
---
+++ Klondike_0.1.0/prototypes/recipes/better_solar_panel_recipe.lua
@@ -0,0 +1,14 @@
+data:extend({
+	{
+		type = "recipe",
+		name = "bettersolar-panel-recipe",
+		category = "smelting",
+		enabled = true,
+		energy_required = 10,
+		ingredients =
+		{
+			{"sand-item", 2},
+		},		
+		result = "glas-item"    	
+	}
+})
