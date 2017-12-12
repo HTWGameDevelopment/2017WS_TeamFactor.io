794675ee207ef5825495c15c5464e2743ab2cf1f
lennart.monir <gartenfreund931@aol.com>
2017-12-12 21:28:00 +0100

    komentar für das event

--- Klondike_0.1.0/events/food/getMeatEvent.lua
+++ Klondike_0.1.0/events/food/getMeatEvent.lua
@@ -4,3 +4,34 @@ script.on_event({defines.events.on_entity_died}, function (e)
         local meat = e.entity.surface.spill_item_stack(e.entity.position, {name="raw-meat-item", count=2}, false)
     end
 end)
+
+--[[
+spill_item_stack(position, items, enable_looted)
+
+Spill items on the ground centered at a given location.
+
+Parameters
+position :: Position: Center of the spillage
+items :: LuaItemStack or SimpleItemStack: Items to spill
+enable_looted :: boolean (optional): When true, each created item will be flagged with the LuaEntity::to_be_looted flag.
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+SimpleItemStack
+
+An item stack may be specified either as a string (in which case it represents a full stack containing the specified item), or as the following table:
+
+    name :: string: Prototype name of the item the stack holds.
+    count :: uint (optional): Number of items the stack holds. If not specified, defaults to 1.
+    health :: float (optional): Health of the items in the stack. Defaults to 1.0.
+    durability :: double (optional): Durability of the tool items in the stack.
+    ammo :: double (optional): Amount of ammo in the ammo items in the stack.
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+to_be_looted :: boolean [Read-Write]
+
+Will this entity be picked up automatically when the player walks over it?
+
+Can only be used if this is ItemEntity
+]]
