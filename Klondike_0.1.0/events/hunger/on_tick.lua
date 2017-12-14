9b0dc49c6b62738ac599960aae297cdf7407eed7
SvenCarstenRasmusen <sven@home.lu>
2017-12-14 11:45:00 +0100

    Hunger
    Rohes fleisch essen wird nun bestraft mit lebenspunkte abzug

--- Klondike_0.1.0/events/hunger/on_tick.lua
+++ Klondike_0.1.0/events/hunger/on_tick.lua
@@ -6,7 +6,8 @@ script.on_event(defines.events.on_tick, function(event)
 			if player.connected then
 				create_player_data(index)
 				player_property_update("increase", index, "ticks", 1)
-				if global.klondike.player[index].ticks % HUNGER_PERIODIC_TICK == 0 then
+				if global.klondike.player[index].ticks % 10 == 0 then
+					--if global.klondike.player[index].ticks % HUNGER_PERIODIC_TICK == 0 then
 					if global.klondike.player[index].hunger_value > 0 then
 						player_property_update("decrease", index, "hunger_value", HUNGER_DECREASE)
 						hunger_gui_check(index)
@@ -15,17 +16,19 @@ script.on_event(defines.events.on_tick, function(event)
 					end
 				player_fatigue(index, global.klondike.player[index].hunger_value)
 				end
-				if global.klondike.player[index].ticks % HEALTH_PERIODIC_TICK == 0 then
-					if global.klondike.player[index].hunger_value == 0 and player.character ~= nil then
-						if DIFFICULTY_VALUE == 0 then
-							if player.character.health > HEALTH_DECREASE then
-								player_damage(index, HEALTH_DECREASE)
-							end
-						else
-							player_damage(index, HEALTH_DECREASE)
-						end
-					end
-				end
+
+				--Healthpoints loss when hungry
+				--if global.klondike.player[index].ticks % HEALTH_PERIODIC_TICK == 0 then
+				--	if global.klondike.player[index].hunger_value == 0 and player.character ~= nil then
+				--		if DIFFICULTY_VALUE == 0 then
+				--			if player.character.health > HEALTH_DECREASE then
+				--				player_damage(index, HEALTH_DECREASE)
+				--			end
+				--		else
+				--			player_damage(index, HEALTH_DECREASE)
+				--		end
+				--	end
+				--end
 			end
 		end
 	end
