c36b119e5401bdd8b1ab08742048980984f81db9
lennart.monir <gartenfreund931@aol.com>
2018-01-05 20:47:40 +0100

    update

--- Klondike_0.1.0/events/hunger/on_built_entity.lua
+++ Klondike_0.1.0/events/hunger/on_built_entity.lua
@@ -4,7 +4,5 @@ script.on_event(defines.events.on_built_entity, function(event)
 	local player = game.players[event.player_index]
 	player_property_update("decrease", event.player_index, "hunger_value", HUNGER_DECREASE_DEPEND_BUILD)
 	hunger_gui_check(event.player_index)
-	if global.klondike.player[event.player_index].hunger_value == 0 and player.character ~= nil then
-		--player_damage(event.player_index, HEALTH_DECREASE_DEPEND_BUILD)
-	end
+	
 end)
--- Klondike_0.1.0/events/hunger/on_player_crafted_item.lua
+++ Klondike_0.1.0/events/hunger/on_player_crafted_item.lua
@@ -4,7 +4,5 @@ script.on_event(defines.events.on_player_crafted_item, function(event)
 	local player = game.players[event.player_index]
 	player_property_update("decrease", event.player_index, "hunger_value", HUNGER_DECREASE_DEPEND_CRAFT)
 	hunger_gui_check(event.player_index)
-	if global.klondike.player[event.player_index].hunger_value == 0 and player.character ~= nil then
-		--player_damage(event.player_index, HEALTH_DECREASE_DEPEND_CRAFT)
-	end
+	
 end)
