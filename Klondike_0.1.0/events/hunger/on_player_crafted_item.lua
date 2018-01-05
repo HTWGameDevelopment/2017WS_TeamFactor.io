b28af06df5d567d5a75d9d57ff487afd91066d2a
lennart.monir <gartenfreund931@aol.com>
2018-01-05 20:16:55 +0100

    update

--- Klondike_0.1.0/events/hunger/on_player_crafted_item.lua
+++ Klondike_0.1.0/events/hunger/on_player_crafted_item.lua
@@ -5,6 +5,6 @@ script.on_event(defines.events.on_player_crafted_item, function(event)
 	player_property_update("decrease", event.player_index, "hunger_value", HUNGER_DECREASE_DEPEND_CRAFT)
 	hunger_gui_check(event.player_index)
 	if global.klondike.player[event.player_index].hunger_value == 0 and player.character ~= nil then
-		player_damage(event.player_index, HEALTH_DECREASE_DEPEND_CRAFT)
+		--player_damage(event.player_index, HEALTH_DECREASE_DEPEND_CRAFT)
 	end
 end)
