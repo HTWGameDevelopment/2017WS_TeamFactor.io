1bf5ad38ac566e2fb826c806a6c433eeb55acb9d
lennart.monir <gartenfreund931@aol.com>
2018-01-06 18:36:02 +0100

    update

--- Klondike_0.1.0/prototypes/items/gold/fast_processing_unit_item.lua
+++ Klondike_0.1.0/prototypes/items/gold/fast_processing_unit_item.lua
@@ -4,7 +4,7 @@ data:extend({
 		name = "fast-processing-unit-item",
 		icon = "__Klondike__/graphics/icons/fast-processing-unit.png",
 		flags = {"goes-to-main-inventory"},
-		order = "e[fast-processing-unit]",
+		order = "g[fast-processing-unit]",
 		subgroup = "intermediate-product",				
 		stack_size = 200
 	}
